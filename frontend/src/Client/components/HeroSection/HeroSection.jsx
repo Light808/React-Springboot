f5f082d4ab1e4565f0c19e38305dae90e62a2620
Light808 <178133326+Light808@users.noreply.github.com>
2025-10-07 17:35:00 +0700

    Update HeroSection.jsx

--- frontend/src/Client/components/HeroSection/HeroSection.jsx
+++ frontend/src/Client/components/HeroSection/HeroSection.jsx
@@ -10,12 +10,12 @@ const HeroSection = () => {
       <div className={`${styles['hero-background']}`}>
         <div className={`${styles['hero-content']}`}>
           <div className={`${styles['hero-text']}`}>
-            <h1 className={`${styles['hero-title']}`}>{t('Welcometocinema')}</h1>
-            <p className={`${styles['hero-subtitle']}`}>{t('Ticketbookingplatform')}</p>
+            <h1 className={`${styles['hero-title']}`}>{t('Welcome to cinema')}</h1>
+            <p className={`${styles['hero-subtitle']}`}>{t('Vietnam\'s leading movie ticket booking platform')}</p>
             <div className={`${styles['hero-actions']}`}>
               <button className={`${styles['hero-btn']} ${styles['primary']}`}>
                 <Play size={20} />
-                {t('Discovernow')}
+                {t('Explore Now')}
               </button>
             </div>
           </div>
